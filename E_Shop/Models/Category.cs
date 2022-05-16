8b9982f64b2be0ef4140042f802b2f5f8f9255e6
PhucSmRePo <0306191060@caothang.edu.vn>
2022-05-16 17:59:11 +0700

    Úpdate namespace

--- E_Shop/Models/Category.cs
+++ E_Shop/Models/Category.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace DA_TotNghiep.Models
+namespace E_Shop_DATN.Models
 {
     public class Category
     {
